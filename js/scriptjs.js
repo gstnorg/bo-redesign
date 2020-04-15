3f2461a2a42ddc70406fdae02b5ae96a9e1ee115
varun kumar <varunkumarmca@gmail.com>
2020-04-15 14:45:04 +0530

    11
    11

---
+++ js/scriptjs.js
@@ -0,0 +1,15 @@
+$(function() {
+    $(".db").on('click', function(e) {
+       e.preventDefault(); // in chase you change to a link or button
+       var $this = $(this),URL = $this.data("id");
+       window.open(window.location+URL, '_self', false);
+    });
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+  });
